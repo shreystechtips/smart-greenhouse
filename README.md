5e1515456b242e31ed59f368da28097d02d380f5
Shrey A <Shreystechtips@gmail.com>
2020-07-19 14:59:37 -0700

    add readme and better plotting

---
+++ README.md
@@ -0,0 +1,9 @@
+# Smart Greenhouse
+
+Using a Raspberry Pi and a few sensors to manage a greenhouse
+
+Uses the Si7021 Temperature and Humidity sensor and SPG30 Gas Sensor (eC02 and TVOC)
+
+## Example Plot output from plotly
+
+[H](https://raw.githubusercontent.com/shreystechtips/smart-greenhouse/master/plot.html)
--- plot.py
+++ plot.py
@@ -4,19 +4,21 @@ from plotly.subplots import make_subplots
 import json 
 from datetime import datetime
 
+
 fig = make_subplots(specs=[[{"secondary_y": True}]])
 
-def plot(input):
+def plot(input,outfile):
     total = [[],{'co2':[],'humid':[],'tvoc':[],'temp':[]}]
     for k,v in input["sensordata"].items():
-        if isinstance(v,dict) and "temp" in v:   
+        if isinstance(v,dict) :   
+            # and v["temp"] >= 33.7
             total[0].append(datetime.strptime(k,"%Y-%m-%dT%H:%M:%S"))
-            # total[1]["co2"].append(v["co2"])
+            total[1]["co2"].append(v["co2"])
             total[1]["temp"].append(v["temp"])
             total[1]["humid"].append(v["humid"])
             total[1]["tvoc"].append(v["tvoc"])
     for k,v in total[1].items():
-        fig.add_trace((go.Scatter(x=total[0], y=v, name=k)))
-    # fig = go.Figure(data=go.Scatter(x=total[0], y=total[1]))
-    fig.show()
-plot(json.loads(open("greenhouse-85aa5-export.json","r").read()))
+        fig.add_trace((go.Scatter(x=total[0], y=v, name=k)), secondary_y = (k=='co2' or k == 'tvoc'))
+    fig.write_html(outfile)
+    # fig.show()
+plot(json.loads(open("greenhouse-85aa5-export.json","r").read()),'plot.html')
